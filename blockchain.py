ab72b3f08d61f716c169480ca2400b7ced5b7141
Jio C. Buaron <jiobuaron17@gmail.com>
2018-08-15 17:11:53 +0800

    removed blockchain.py

--- blockchain.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-
-class Blockchain(object):
-    def __init__(self):
-        self.chain = []
-        self.nodes = set()
-        self.currentNode = sys.argv[1]
-        self.pendingTransactions = []
